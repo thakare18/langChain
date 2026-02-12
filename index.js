e1f7c11a7c888bac0e9499938c6971d9dc2bc5ee
Prathamesh  Thakare <prathamthackeray18@gmail.com>
2026-02-13 00:53:03 +0530

    PromptTemplate integration

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {config} from 'dotenv';
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import {PromptTemplate} from "@langchain/core/prompts"
+
 
 
 config();
@@ -14,4 +16,17 @@ const model = new ChatGoogleGenerativeAI({
 
 // chat creation
 
-model.invoke("best google gemini model for coding").then(console.log)
+// model.invoke("how to get off campus job").then(response => {
+//     console.log(response.content);
+// })
+
+
+// prompt template creation 
+
+const promptTemplate = PromptTemplate.fromTemplate(
+    `explain {topic} in a very simple way like ELI5, make sure to include a core concept and avoid unnecessary jargon. make the answer an concise as possible.`
+)
+
+promptTemplate.pipe(model).invoke({topic: "express"}).then(response => {
+    console.log(response);
+})
